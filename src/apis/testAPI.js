e43c1abb08ddbe6b2b7800ab1c26327e57b00d0a
zhouyoujun <a390687273@qq.com>
2023-08-25 04:09:33 +0800

    删除不需要的代码

--- src/apis/testAPI.js
+++
@@ -1,7 +0,0 @@
-import httpInstance from '@/utils/http.js'
-
-export function getCategory() {
-  return  httpInstance({
-    url:'home/category/head'
-  })
-}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 //引入初始化样式文件
 import '@/styles/common.scss'
+
 import ElementPlus from 'element-plus'
 // import './assets/main.css'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-//测试接口函数
-// import { getCategory } from '@/apis/testAPI.js'
-// getCategory().then(res => {
-//   console.log(res);
-// })
+
 
 const app = createApp(App)
 
