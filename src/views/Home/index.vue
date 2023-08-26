497fe351214b50281f64960be0569a3fd6cd855f
zhouyoujun <a390687273@qq.com>
2023-08-26 13:40:52 +0800

    home-面板组件封装并进行测试

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,8 +4,10 @@ import HomeBanner from "@/views/Home/components/HomeBanner.vue";
 import HomeNew from "@/views/Home/components/HomeNew.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeProduct from "@/views/Home/components/HomeProduct.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 import { useCategoryStore } from "@/stores/category";
 import { onMounted } from "vue";
+
 const categoryStore = useCategoryStore();
 onMounted(() => {
   return categoryStore.getCategory();
@@ -19,4 +21,11 @@ onMounted(() => {
   <HomeNew></HomeNew>
   <HomeHot></HomeHot>
   <HomeProduct></HomeProduct>
+  <!-- 测试 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是插槽1</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是插槽2</div>
+  </HomePanel>
 </template>
