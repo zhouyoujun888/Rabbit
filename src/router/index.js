6b964e05ef763fae586d2ac342dd3c87e7f4b2c5
zhouyoujun <a390687273@qq.com>
2023-08-27 11:00:32 +0800

    一级分类-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,33 @@
 //createRouter:创建router实例对象 createWebHistory:创建history模式的路由
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和component对应关系的位置
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Layout,
       children: [
-      {
-        path: '',
-        component: Home
-      },
-      {
-        path: 'category',
-        component: Category
-      }
-      ]
+        {
+          path: "",
+          component: Home,
+        },
+        {
+          path: "category/:id",
+          component: Category,
+        },
+      ],
     },
     {
-      path: '/login',
-      component:Login
-    }
-  ]
-})
+      path: "/login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
